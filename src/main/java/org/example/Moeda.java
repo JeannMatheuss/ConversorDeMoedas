68793a582be0bd89b9fb323344bdcfa0bbc91b9c
Jean Matheus <jeanmatheus0612@gmail.com>
2025-06-09 22:18:24 -0300

    Criando a classe moeda e seus respectivos métodos

---
+++ src/main/java/org/example/Moeda.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Moeda {
+    private String codigo;
+
+    public Moeda(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
